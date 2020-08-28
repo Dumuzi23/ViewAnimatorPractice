d550d10c41af73fc60265af913c5dd05b8c71b4a
ta <amida.tatsuya@gmail.com>
2020-08-28 19:14:16 +0900

    add-アニメーション機能を追加

--- ViewAnimatorPractice/ViewController.swift
+++ ViewAnimatorPractice/ViewController.swift
@@ -7,11 +7,11 @@
 //
 
 import UIKit
+import ViewAnimator
 
 class ViewController: UITableViewController, UISearchBarDelegate {
 
     @IBOutlet weak var SchBr: UISearchBar!
-    
     
     var repo: [[String: Any]]=[]
     
@@ -20,10 +20,11 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     var url: String!
     var idx: Int!
     
+    let animations = [AnimationType.from(direction: .bottom, offset: 100.0)]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
+
         SchBr.delegate = self
     }
     
@@ -38,8 +39,8 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         word = searchBar.text!
+        
         
         if word.count != 0 {
             url = "https://api.github.com/search/repositories?q=\(word!)"
@@ -49,6 +50,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
                     self.repo = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
+                            UIView.animate(views: self.tableView.visibleCells, animations: self.animations)
                         }
                     }
                 }
@@ -56,31 +58,28 @@ class ViewController: UITableViewController, UISearchBarDelegate {
         // これ呼ばなきゃリストが更新されません
         task?.resume()
         }
-        
+
+    SchBr.resignFirstResponder()
+
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//
-//        if segue.identifier == "Detail"{
-//            let dtl = segue.destination as! ViewController2
-//            dtl.vc1 = self
-//        }
-//
-//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repo.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = UITableViewCell()
         let rp = repo[indexPath.row]
         cell.textLabel?.text = rp["full_name"] as? String ?? ""
         cell.detailTextLabel?.text = rp["language"] as? String ?? ""
         cell.tag = indexPath.row
+
         return cell
         
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        SchBr.resignFirstResponder()
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
